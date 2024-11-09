237979bc74310fcbe5950bf432fc699dae0898cc
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2024-11-08 23:41:09 -0500

    Algorithm simulated annealing

--- Devoir2_INF8175_A24/RechercheLocale/code/solver.py
+++ Devoir2_INF8175_A24/RechercheLocale/code/solver.py
@@ -1,26 +1,29 @@
 import random
 import math
 
-def solve(schedule):
+def simulated_annealing(schedule):
+    """
+    Implements simulated annealing algorithm for academic schedule optimization
+    :param schedule: object describing the input schedule
+    :return: optimized schedule solution
+    """
     initial_temperature = 1000.0
     alpha = 0.95  
     max_iterations = 1000
     
     current_solution = generate_initial_solution(schedule)
-    best_solution = current_solution.copy() # La meilleur solution est la solution initiale au début
-    current_cost = calculate_conflicts_cost(current_solution, schedule) # Évaluation de la solution initiale
-    best_cost = current_cost # Le meilleur coût est le coût de la solution initiale au début
+    best_solution = current_solution.copy()
+    current_cost = calculate_conflicts_cost(current_solution, schedule)
+    best_cost = current_cost
     
     temperature = initial_temperature
     
     for k in range(max_iterations):
-        # Une Deep copie permet de préserver l'état précédant
         neighbor = generate_neighbor(current_solution.copy(), schedule)
         new_cost = calculate_conflicts_cost(neighbor, schedule)
         
         delta = new_cost - current_cost
         
-        # Fonction de sélection de la solution
         if delta <= 0 or random.random() < math.exp(-delta / temperature):
             current_solution = neighbor
             current_cost = new_cost
@@ -35,7 +38,7 @@ def solve(schedule):
 
 def generate_neighbor(solution, schedule):
     """
-    Fonction de voisinage
+    Generates a neighboring solution by making a random modification
     """
     course = random.choice(list(schedule.course_list))
     new_slot = random.randint(1, len(schedule.course_list))
@@ -47,7 +50,6 @@ def generate_neighbor(solution, schedule):
     return solution
 
 def calculate_conflicts_cost(solution, schedule):
-    """ Fonction d'évaluation"""
     conflicts = 0
     for course in schedule.course_list:
         for neighbor in schedule.get_node_conflicts(course):
@@ -57,11 +59,14 @@ def calculate_conflicts_cost(solution, schedule):
 
 
 def generate_initial_solution(schedule):
-    """ Fonction de génération d'une solution initiale"""
     solution = {}
     for course in schedule.course_list:
+        assigned = False
         for time_slot in range(1, len(schedule.course_list) + 1): 
             if all(solution.get(neighbor) != time_slot for neighbor in schedule.get_node_conflicts(course)):
                 solution[course] = time_slot
+                assigned = True
                 break
+        if not assigned:
+            raise Exception(f"Unable to assign a slot for course {course}")
     return solution
